0a8d7665f12ceb57e5fbeeaf799d86f9cd15a69d
jiawei380106 <43428969+jiawei380106@users.noreply.github.com>
2019-03-26 21:15:14 +0800

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase";
+
+let config = {
+  apiKey: "AIzaSyC5tpD6N9zJT3V-xHHqAdrBwKuEfE25GKo",
+  authDomain: "bt3103-f9af7.firebaseapp.com",
+  databaseURL: "https://bt3103-f9af7.firebaseio.com",
+  projectId: "bt3103-f9af7",
+  storageBucket: "bt3103-f9af7.appspot.com",
+  messagingSenderId: "1059333989137"
+};
+
+let app = Firebase.initializeApp(config);
+
+let db = app.database();
+//let seRef = db.ref("se_df/data/0");
+
+export { db };
